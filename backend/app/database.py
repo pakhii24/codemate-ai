5a2f20ca31b95f55c33001d28a39a50ae6850adc
pakhii24 <pakhi.jan24104@gmail.com>
2026-04-13 23:42:36 +0530

    fix database.py env var

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,8 @@
-# backend/app/database.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-DATABASE_URL = os.getenv(DATABASE_URL = "postgresql://postgres:password@postgres:5432/codemate")
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:password@postgres:5432/codemate')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
